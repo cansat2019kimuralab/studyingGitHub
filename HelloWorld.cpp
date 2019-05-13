32f6be80c68566876063f6926c7580d1ca942677
Pasises <45708319+Pasises@users.noreply.github.com>
2019-05-13 12:55:54 +0900

    Add loop 10 times

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int i;
     for(i = 0  ; i < 10 ; i++){
-        cout << "Hello World!\n" << endl;
+        cout << "Hello World!" << endl;
     }
     return 0;
 }
