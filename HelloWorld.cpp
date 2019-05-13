fb812b14e577779a9e06910d8788147c63488be8
Pasises <45708319+Pasises@users.noreply.github.com>
2019-05-13 13:03:14 +0900

    Add number

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     int i;
     for(i = 0  ; i < 10 ; i++){
-        cout << "Hello World!" << endl;
+        cout << i;
+        cout << "  Hello World!" << endl;
     }
     return 0;
 }
